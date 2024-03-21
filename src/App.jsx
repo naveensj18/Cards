5fbd2e836a4100b3e77e5c140308e6f0a5955bb8
naveensj18 <naveenprasanthsa1147@gmail.com>
2024-03-21 11:03:22 +0530

    Added about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from "./pages/HomePage/Home";
 import Game from "./pages/GamePage/Game";
 import Mode from "./pages/Mode/Mode";
 import { Choice } from "./pages/Mode/Choice";
+import { About } from "./pages/About";
 import TwoPlayerMode from "./pages/TwoPlayer/TwoPlayerMode";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
       <Route path="/Mode" element={<Mode />} />
       <Route path="/Game" element={<Choice />} />
       <Route path="/TwoPlayerMode" element={<TwoPlayerMode />} />
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -9,9 +9,12 @@ function Home() {
         <button>Play Online</button>
       </Link>
       <Link className="play-with-friends-button">
-        <button onClick={() => window.alert("This mode not available yet")}>
+        <button onClick={() => window.alert("This mode is not available yet")}>
           Play with Friends
         </button>
+      </Link>
+      <Link to="/About" className="about-link">
+        About
       </Link>
     </div>
   );
